d9c22a184b181c10f73fcbc3d81f0533849d546f
software <kavib@officebrain.com>
2017-11-03 19:15:24 +0530

    first commit

--- FlowzSendEmailService/config/smtp.js
+++
@@ -1,9 +0,0 @@
-const smtp = {
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // secure:true for port 465, secure:false for port 587
-  auth: {
-      user: 'xxxxxxxxx@gmail.com',
-      pass: 'xxxxxxxxxxxx'
-  }
-}
