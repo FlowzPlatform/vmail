4e7c467b83cc9d11b13e98f754481a36cbde7e7e
software <kavib@officebrain.com>
2017-11-16 14:54:29 +0530

    Count issue and mjml template editor issue is resolved

--- vmailSeneca/node_modules/mjml-accordion/src/index.js
+++
@@ -1,6 +0,0 @@
-import Accordion from './Accordion'
-import AccordionElement from './AccordionElement'
-import AccordionTitle from './AccordionTitle'
-import AccordionText from './AccordionText'
-
-export default { Accordion, AccordionElement, AccordionTitle, AccordionText }
