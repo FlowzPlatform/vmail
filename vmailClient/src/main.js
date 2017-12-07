bd9c398bdddf206f2f6242a859c7854b172fc244
software <kavib@officebrain.com>
2017-12-07 11:18:19 +0530

    calendar language issue resolved

--- vmailClient/src/main.js
+++ vmailClient/src/main.js
@@ -4,12 +4,13 @@ import router from './router'
 import {store} from './store'
 import VModal from 'vue-js-modal'
 import iView from 'iview'
-import enLocale from 'iview/src/locale/lang/en-US'
+// import enLocale from 'iview/src/locale/lang/en-US'
+import locale from 'iview/dist/locale/en-US'
 import 'iview/dist/styles/iview.css'
 var VueCookie = require('vue-cookie')
 
 Vue.use(VModal, { dialog: true })
-Vue.use(iView,{enLocale})
+Vue.use(iView,{locale})
 Vue.config.productionTip = false
 Vue.use(VueCookie)
 
