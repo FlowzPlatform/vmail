792f65a3a3c5a576408768522ed710ac79d09e0b
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-03-07 22:19:43 +0530

    staging-files-move-to-develop

--- upgrade.sh
+++ upgrade.sh
@@ -53,6 +53,17 @@ then
       SENECAURL="$SENECAURL_DEVELOP";
       FRONT_HOST="$FRONT_HOST_DEVELOP";
   }
+elif [ "$TRAVIS_BRANCH" = "staging" ]
+then
+    {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Staging" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      USERNAME="$DOCKER_USERNAME";
+      TAG="staging";
+      SENECAURL="$SENECAURL_STAGING";
+      FRONT_HOST="$FRONT_HOST_STAGING";
+  }  
 else
   {
       echo "call $TRAVIS_BRANCH branch"
@@ -82,7 +93,7 @@ curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{
-  "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/mail_seneca_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["4000:4000/tcp"],"environment": {"smtpHost": "'"$SMTPHOST"'","smtpPort": "'"$SMTPPORT"'","user": "'"$USER"'","password": "'"$PASSWORD"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 4000,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+  "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/mail_seneca_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["4000:4000/tcp"],"environment": {"smtphost": "'"$SMTPHOST"'","smtpPort": "'"$SMTPPORT"'","user": "'"$USER"'","password": "'"$PASSWORD"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 4000,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
 http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_SENECA?action=upgrade
 
 curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
