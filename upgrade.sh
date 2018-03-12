1d137d8b01a7a5fd4fb7ff715488a48f00bc9f5b
80017 <arpitap@officebeacon.com>
2018-03-12 12:52:19 +0530

    change ci-cd code

--- upgrade.sh
+++ upgrade.sh
@@ -35,60 +35,72 @@ if [ "$TRAVIS_BRANCH" = "master" ]
 then
     {
     echo "call $TRAVIS_BRANCH branch"
-    ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Production" | jq '.data[].id' | tr -d '"'`
+    ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_MASTER":"$RANCHER_SECRETKEY_MASTER"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_MASTER/v2-beta/projects?name=Production" | jq '.data[].id' | tr -d '"'`
     echo $ENV_ID
     USERNAME="$DOCKER_USERNAME_FLOWZ";
     TAG="latest";
     SENECAURL="$SENECAURL_MASTER";
     FRONT_HOST="$FRONT_HOST_MASTER";
+    RANCHER_ACCESSKEY="$RANCHER_ACCESSKEY_MASTER";
+    RANCHER_SECRETKEY="$RANCHER_SECRETKEY_MASTER";
+    RANCHER_URL="$RANCHER_URL_MASTER";
   }
 elif [ "$TRAVIS_BRANCH" = "develop" ]
 then
     {
       echo "call $TRAVIS_BRANCH branch"
-      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Develop" | jq '.data[].id' | tr -d '"'`
+      ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_DEVELOP":"$RANCHER_SECRETKEY_DEVELOP"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_DEVELOP/v2-beta/projects?name=Develop" | jq '.data[].id' | tr -d '"'`
       echo $ENV_ID
       USERNAME="$DOCKER_USERNAME";
       TAG="dev";
       SENECAURL="$SENECAURL_DEVELOP";
       FRONT_HOST="$FRONT_HOST_DEVELOP";
+      RANCHER_ACCESSKEY="$RANCHER_ACCESSKEY_DEVELOP";
+      RANCHER_SECRETKEY="$RANCHER_SECRETKEY_DEVELOP";
+      RANCHER_URL="$RANCHER_URL_DEVELOP";
   }
 elif [ "$TRAVIS_BRANCH" = "staging" ]
 then
     {
       echo "call $TRAVIS_BRANCH branch"
-      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=Staging" | jq '.data[].id' | tr -d '"'`
+      ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_STAGING":"$RANCHER_SECRETKEY_STAGING"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_STAGING/v2-beta/projects?name=Staging" | jq '.data[].id' | tr -d '"'`
       echo $ENV_ID
       USERNAME="$DOCKER_USERNAME";
       TAG="staging";
       SENECAURL="$SENECAURL_STAGING";
       FRONT_HOST="$FRONT_HOST_STAGING";
+      RANCHER_ACCESSKEY="$RANCHER_ACCESSKEY_STAGING";
+      RANCHER_SECRETKEY="$RANCHER_SECRETKEY_STAGING";
+      RANCHER_URL="$RANCHER_URL_STAGING";
   }  
 else
   {
       echo "call $TRAVIS_BRANCH branch"
-      ENV_ID=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects?name=QA" | jq '.data[].id' | tr -d '"'`
+      ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_QA":"$RANCHER_SECRETKEY_QA"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_QA/v2-beta/projects?name=QA" | jq '.data[].id' | tr -d '"'`
       echo $ENV_ID
       USERNAME="$DOCKER_USERNAME";
       TAG="qa";
       SENECAURL="$SENECAURL_QA";
       FRONT_HOST="$FRONT_HOST_QA";
+      RANCHER_ACCESSKEY="$RANCHER_ACCESSKEY_QA";
+      RANCHER_SECRETKEY="$RANCHER_SECRETKEY_QA";
+      RANCHER_URL="$RANCHER_URL_QA";
   }
 fi
 
-SERVICE_ID_SENECA=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services?name=vmail-seneca-service" | jq '.data[].id' | tr -d '"'`
+SERVICE_ID_SENECA=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL/v2-beta/projects/$ENV_ID/services?name=vmail-seneca-service" | jq '.data[].id' | tr -d '"'`
 echo $SERVICE_ID_SENECA
 
-SERVICE_ID_MICRO=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services?name=vmail-micro-service" | jq '.data[].id' | tr -d '"'`
+SERVICE_ID_MICRO=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL/v2-beta/projects/$ENV_ID/services?name=vmail-micro-service" | jq '.data[].id' | tr -d '"'`
 echo $SERVICE_ID_MICRO
 
-SERVICE_ID_BACKEND=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services?name=vmail-backend-service" | jq '.data[].id' | tr -d '"'`
+SERVICE_ID_BACKEND=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL/v2-beta/projects/$ENV_ID/services?name=vmail-backend-service" | jq '.data[].id' | tr -d '"'`
 echo $SERVICE_ID_BACKEND
 
-SERVICE_ID_FRONTEND=`curl -u ""$RANCHER_USER":"$RANCHER_PASS"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services?name=vmail-frontend-service" | jq '.data[].id' | tr -d '"'`
+SERVICE_ID_FRONTEND=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL/v2-beta/projects/$ENV_ID/services?name=vmail-frontend-service" | jq '.data[].id' | tr -d '"'`
 echo $SERVICE_ID_FRONTEND
 
-curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
@@ -96,25 +108,25 @@ curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
   "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/mail_seneca_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["4000:4000/tcp"],"environment": {"smtphost": "'"$SMTPHOST"'","smtpPort": "'"$SMTPPORT"'","user": "'"$USER"'","password": "'"$PASSWORD"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 4000,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
 $RANCHER_URL/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_SENECA?action=upgrade
 
-curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{"inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/mail_email_services_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3003:3003/tcp"],"environment": {"rdb": "'"$RDB"'","rhost": "'"$RDB_HOST"'","rport": "'"$RDB_PORT"'","senecaurl": "'"$SENECAURL"'","privatekey": "'"$PRIVATEKEY"'","awsaccesskey":"'"$AWSACCESSKEY"'","awsprivatekey":"'"$AWSPRIVATEKEY"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3003,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
-http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_MICRO?action=upgrade
+$RANCHER_URL/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_MICRO?action=upgrade
 
-curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{
   "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/mail_backend_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3036:3036/tcp","4036:4036/tcp"],"environment": {"rdb": "'"$RDB"'","rhost": "'"$RDB_HOST"'","rport": "'"$RDB_PORT"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3036,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
-http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_BACKEND?action=upgrade
+$RANCHER_URL/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_BACKEND?action=upgrade
 
-curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{
      "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/mail_frontend_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "'"$FRONT_HOST"'","io.rancher.scheduler.affinity:container_label_soft_ne": "io.rancher.stack_service.name=front-flowz/vmail-frontend-service"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 80,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"requestLine": "GET \"http://localhost\" \"HTTP/1.0\"","responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
-http://rancher.flowz.com:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_FRONTEND?action=upgrade
+$RANCHER_URL/v2-beta/projects/$ENV_ID/services/$SERVICE_ID_FRONTEND?action=upgrade
